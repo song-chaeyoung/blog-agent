ac98e3d9256f54a999435be1f3b05e3c0c58da6b
SongChaeyoung <call0512@naver.com>
2026-02-10 16:48:22 +0900

    CHA-50 : Next.js 프로젝트 생성 및 기본 환경 설정

---
+++ src/app/providers.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+import { ClerkProvider, useAuth } from "@clerk/nextjs";
+import { ConvexProviderWithClerk } from "convex/react-clerk";
+import { ConvexReactClient } from "convex/react";
+
+const convex = new ConvexReactClient(
+  process.env.NEXT_PUBLIC_CONVEX_URL as string
+);
+
+export default function Providers({ children }: { children: React.ReactNode }) {
+  return (
+    <ClerkProvider>
+      <ConvexProviderWithClerk client={convex} useAuth={useAuth}>
+        {children}
+      </ConvexProviderWithClerk>
+    </ClerkProvider>
+  );
+}
